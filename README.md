adf4c0d9feac6f816394d3a9c999511f587624dc
XingKong <X.K.Sync@gmail.com>
2021-03-21 01:37:13 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,59 @@
+# XingKongScreenSaver
+
+## 一、简介
+在采用 `树莓派4B + 微雪 4inch DPI LCD` 的组合来实现项目展示的时候，发现 Raspbian 系统的屏幕保护功能无法正常工作，若用户在一段时间之内没有操作，系统无法自动关闭屏幕
+
+在苦苦搜索无果后，决定自己写一个程序，实现空闲时自动息屏，用户操作时自动亮屏功能
+
+推测可能与 DPI 屏幕有关，HDMI 屏幕应该无此问题
+
+## 二、安装
+`xkscreensaver` 需要依赖 `xprintidle` 才能正常运行
+```Shell
+sudo apt-get install xprintidle
+```
+
+编译或下载 `xkscreensaver` 可执行文件，拷贝到 `/home/pi/` 目录下
+>注意：<br>
+直接编译完成后的文件名为 `XKScreenSaver`，为了方便期间，建议重命名为小写 `xkscreensaver`
+
+然后运行
+
+```Shell
+sudo nano /usr/lib/systemd/system/xkscreensaver.service
+```
+
+在文本编辑器中输入下面的内容，保存后退出
+
+```Shell
+[Unit]
+Description=xkscreensaver
+
+[Service]
+Type=simple
+User=pi
+Group=pi
+LimitNOFILE=100000
+LimitNPROC=100000
+ExecStart=/home/pi/xkscreensaver
+StandardOutput=null
+
+[Install]
+WantedBy=multi-user.target
+
+```
+
+通知系统重新加载服务
+```Shell
+sudo systemctl daemon-reload
+```
+
+设置服务为开机自启
+```Shell
+sudo systemctl enable xkscreensaver
+```
+
+重新启动树莓派
+```Shell
+sudo reboot
+```
---
+++ XKScreenSaver/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace XKScreenSaver
+{
+    enum ScreenState
+    {
+        Unknown,
+        On,
+        Off
+    }
+
+    class Program
+    {
+        const int CONST_TIME_WAIT = 2 * 60 * 1000;//2 minitues for auto turn off screen
+
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("DISPLAY", ":0");
+
+            if (args == null || args.Length == 0)
+            {
+                DoScreenSave();
+            }
+            else if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "-t":
+                        Test();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please ensure xprintidle has installed on your system.");
+                Console.WriteLine("Usage: .\\XKScreenSaver -t To test if dependency has installed properly.");
+            }
+        }
+
+        static void Test()
+        {
+            try
+            {
+                ulong idleTime = GetIdleTime();
+                Console.WriteLine($"Idle: {idleTime / 1000}s");
+
+                Console.WriteLine("It seems ok.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Please check if you have installed xprintidle");
+            }
+        }
+
+        static void DoScreenSave()
+        {
+            ScreenState screenState = ScreenState.Unknown;
+            while (true)
+            {
+                try
+                {
+                    ulong idleTime = GetIdleTime();
+                    //Console.WriteLine($"Idle: {idleTime / 1000}s");
+                    if (idleTime > CONST_TIME_WAIT)
+                    {
+                        if (screenState != ScreenState.Off)
+                        {
+                            OnOffScreen(false);
+                            screenState = ScreenState.Off;
+                        }
+                    }
+                    else
+                    {
+                        if (screenState != ScreenState.On)
+                        {
+                            OnOffScreen(true);
+                            screenState = ScreenState.On;
+                        }
+                    }
+
+                    Thread.Sleep(500);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        static ulong GetIdleTime()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("xprintidle");
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            Process proc = Process.Start(psi);
+            string strIdleTime = proc.StandardOutput.ReadToEnd();
+            return ulong.Parse(strIdleTime);
+        }
+
+        static void OnOffScreen(bool on)
+        {
+            int arg = on ? 1 : 0;//0: off 1: on
+            Process.Start("vcgencmd", $"display_power {arg}");
+        }
+    }
+}
